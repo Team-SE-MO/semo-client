0364a5e7f9dd7dabead3658f61d607042ed0e047
taeyeon721 <garong0721@gmail.com>
2024-10-22 16:01:10 +0900

    ♻️ refactor: 기존 크기 수정 및 아이콘 포함된 스타일 추가

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
 import './Button.scss';
+import { SvgIconComponent } from '@mui/icons-material';
+import { SvgIcon } from '@mui/material';
 
+/*
+  아이콘 있는 버튼 예시
+  // 부모 컴포넌트에서 아이콘 import
+  import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+  <Button
+    icon={LockOutlinedIcon}
+    label="&nbsp;&nbsp;비밀번호 변경"
+    size="large"
+    radius="rounded"
+    disalbed
+    color="disable"
+    type="button"
+  />
+*/
 interface ButtonProps {
-  size?: 'small' | 'medium' | 'large' | 'xlarge';
-  label: string;
-  color?: 'primary' | 'danger' | 'success' | 'other';
+  size: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge';
+  label?: string;
+  color: 'primary' | 'danger' | 'success' | 'disable' | 'other';
   radius?: 'rounded' | 'pill' | 'oval';
   shadow?: boolean;
   type: 'button' | 'submit';
-  // TODO: disabled 추가
+  icon?: SvgIconComponent;
+  disalbed?: boolean;
   onClick?: () => void;
 }
 
 const Button = ({
   size = 'medium',
-  label,
+  label = '',
   color = 'primary',
   radius = 'pill',
   shadow = false,
   type,
+  icon,
+  disalbed = false,
   ...props
 }: ButtonProps) => {
   const neumorphism = shadow ? 'button--shadow' : '';
@@ -32,8 +52,12 @@ const Button = ({
         `button--${radius}`,
         neumorphism,
       ].join(' ')}
+      disabled={disalbed}
       {...props}
     >
+      {icon && (
+        <SvgIcon className="button__icon" component={icon} inheritViewBox />
+      )}
       {label}
     </button>
   );
