1063e184a57e706d0c243fe4334102f95efa441a
taeyeon721 <garong0721@gmail.com>
2024-10-23 10:09:03 +0900

    🍎 chore: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import './reset.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainLayout from 'pages/MainLayout';
+import Main from 'pages/Main';
+import Login from 'pages/Login';
+import SignUp from 'pages/SignUp';
+import UserRequests from 'pages/UserRequests';
+import CompanyRequests from 'pages/CompanyRequests';
+import ForgetPassword from 'pages/ForgetPassword';
+import Devices from 'pages/Devices';
+import Users from 'pages/Users';
+import CompanyUsers from 'pages/CompanyUsers';
+import Summary from 'pages/Summary';
+import Dashboard from 'pages/Dashboard';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Main />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/user-req" element={<UserRequests />} />
+            <Route path="/company-req" element={<CompanyRequests />} />
+            <Route path="/forget-password" element={<ForgetPassword />} />
+            <Route path="/devices" element={<Devices />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/users/:companyid" element={<CompanyUsers />} />
+            <Route path="/summary" element={<Summary />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
