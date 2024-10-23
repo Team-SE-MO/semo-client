9ee18ee0e2412f77366393367f5c3bdb5daee64e
JUNI <junehee107@naver.com>
2024-10-23 16:43:10 +0900

    ✨ feat: 메인페이지 구현

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Welcome from 'components/organisms/welcome/Welcome';
+import './Main.scss';
 
 const Main = () => {
-  return <div />;
+  return (
+    <div className="main-container">
+      <Welcome />
+    </div>
+  );
 };
 
 export default Main;
