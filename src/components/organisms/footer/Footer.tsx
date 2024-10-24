8a6e457432aa1056f41144b3a0444df99e8bdf32
JUNI <junehee107@naver.com>
2024-10-25 02:30:24 +0900

    ✨ feat: footer, mainLayout 구현

---
+++ src/components/organisms/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.scss';
+import Text from 'components/atoms/text/Text';
+
+export const Footer = () => {
+  const content =
+    '10:00 ~ 18:00 (점심 시간 13:00 ~ 14:00 ) \n 주일, 공유일 휴무';
+  return (
+    <div className="footer__container">
+      <div className="footer__logo" />
+      <Text text={content} type="info" color="neutral" />
+    </div>
+  );
+};
--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from 'components/organisms/footer/Footer';
 import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 
@@ -6,6 +7,7 @@ const MainLayout = () => {
   return (
     <div className="main__layout-body">
       <Outlet />
+      <Footer />
     </div>
   );
 };
