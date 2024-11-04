eeb5ec5190c26a392e8cd85a2797531327411a0f
taeyeon721 <garong0721@gmail.com>
2024-11-04 18:16:35 +0900

    ✨ feat: total summary device 완성

--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Text from 'components/atoms/text/Text';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
+import type Device from '../types/Device';
 import './Summary.scss';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
@@ -19,46 +20,43 @@ const Summary = () => {
         LOCALHOST7: 26,
       },
       warnDevice: {
-        // "LOCALHOST21": 3 // BLOKING SESSION 3개
+        // LOCALHOST21: 3,
+        // BLOKING SESSION 3개
       },
-      unUsedDevice: {
-        // "LOCALHOST22": 55 // 55분 전
-      },
-    },
-    allDevices: {
-      LOCALHOST19: {
-        type: 'ORACLE',
-        status: 'ACTIVE',
-        sid: 'XE',
-        ip: '127.0.0.1',
-        port: 1521,
-        statusValue: 24,
-      },
-      LOCALHOST18: {
-        type: 'ORACLE',
-        status: 'ACTIVE',
-        sid: 'XE',
-        ip: '127.0.0.1',
-        port: 1521,
-        statusValue: 24,
-      },
-      LOCALHOST17: {
-        type: 'ORACLE',
-        status: 'ACTIVE',
-        sid: 'XE',
-        ip: '127.0.0.1',
-        port: 1521,
-        statusValue: 24,
-      },
-      LOCALHOST16: {
-        type: 'ORACLE',
-        status: 'ACTIVE',
-        sid: 'XE',
-        ip: '127.0.0.1',
-        port: 1521,
-        statusValue: 24,
+      unusedDevice: {
+        LOCALHOST22: 55,
+        // 55분 전
       },
     },
+    allDevices: [
+      {
+        deviceAlias: 'LOCALHOST12',
+        type: 'ORACLE',
+        status: 'ACTIVE',
+        sid: 'XE',
+        ip: '127.0.0.1',
+        port: 1521,
+        statusValue: 30,
+      },
+      {
+        deviceAlias: 'LOCALHOST14',
+        type: 'ORACLE',
+        status: 'ACTIVE',
+        sid: 'XE',
+        ip: '127.0.0.1',
+        port: 1521,
+        statusValue: 30,
+      },
+      {
+        deviceAlias: 'LOCALHOST3',
+        type: 'ORACLE',
+        status: 'ACTIVE',
+        sid: 'XE',
+        ip: '127.0.0.1',
+        port: 1521,
+        statusValue: 30,
+      },
+    ],
   };
   const { companyName } = data;
   const activeCnt = data.totalProcessInfo.activeDeviceCnt;
@@ -97,6 +95,10 @@ const Summary = () => {
     },
   };
   const { topUsedDevices } = data.totalProcessInfo;
+  const { warnDevice } = data.totalProcessInfo;
+  const { unusedDevice } = data.totalProcessInfo;
+  const { allDevices } = data;
+
   return (
     <div className="summary__container">
       <div className="summary__title">
@@ -114,10 +116,10 @@ const Summary = () => {
             <Doughnut data={dougnutData} options={dougnutOpt} />
           </div>
         </div>
-        <div className="summary__overview__most-used">
+        <div className="summary__card">
           <Text content="현재 가장 많이 사용되고 있는 DB" type="info" />
           {Object.keys(topUsedDevices).map((item) => (
-            <div>
+            <div className="summary__card__db">
               <Text content={item} type="info" />
               <Text
                 content={`${topUsedDevices[item as keyof typeof topUsedDevices]}`}
@@ -126,8 +128,59 @@ const Summary = () => {
             </div>
           ))}
         </div>
-        <div>
+        <div className="summary__card">
           <Text content="주의 요망 DB" type="info" />
+          {Object.entries(warnDevice).length > 0 ? (
+            <div>
+              {Object.keys(warnDevice).map((item) => (
+                <div className="summary__card__db">
+                  <Text content={item} type="info" />
+                  <Text
+                    content={`${warnDevice[item as keyof typeof warnDevice]}`}
+                    type="info"
+                  />
+                </div>
+              ))}
+            </div>
+          ) : (
+            <Text content="내역이 없습니다." type="info" color="neutral" />
+          )}
+        </div>
+        <div className="summary__card">
+          <Text content="사용한 지 오래된 DB" type="info" />
+          {Object.entries(unusedDevice).length > 0 ? (
+            <div>
+              {Object.keys(unusedDevice).map((item) => (
+                <div className="summary__card__db">
+                  <Text content={item} type="info" />
+                  <Text
+                    content={`${Math.trunc(unusedDevice[item as keyof typeof unusedDevice] / 1440)}일 전`}
+                    type="info"
+                  />
+                </div>
+              ))}
+            </div>
+          ) : (
+            <Text content="내역이 없습니다." type="info" color="neutral" />
+          )}
+        </div>
+      </div>
+      <div>
+        <Text content="전체 DB" type="subtitle" bold />
+        <div className="device-list">
+          {allDevices.map((item) => (
+            <div
+              key={item.deviceAlias}
+              className={['device-card', `device-card--${item.status}`].join(
+                ' '
+              )}
+            >
+              <Text content={item.deviceAlias} type="info" />
+              <Text content={item.sid} type="link" />
+              <Text content={item.ip} type="link" />
+              <Text content={`${item.port}`} type="link" />
+            </div>
+          ))}
         </div>
       </div>
     </div>
