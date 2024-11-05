e78ba4f32adee2305861f9d1b615cfea80e35122
dabeen-jung <ltea99@naver.com>
2024-11-05 12:00:25 +0900

    ✨ feat: 데이터베이스 삭제 모달 구현

--- src/components/organisms/modal/DatabaseDelete.tsx
+++ src/components/organisms/modal/DatabaseDelete.tsx
@@ -10,7 +10,70 @@ interface DatabaseDeleteProps {
 }
 
 const DatabaseDelete = ({ isOpen, onClose }: DatabaseDeleteProps) => {
-  return <div>DatabaseDelete</div>;
+  const handleSubmit = async () => {
+    // TODO : axios 인터페이스 설정 시 해당 요청 api 연결
+    onClose();
+  };
+
+  if (!isOpen) return null;
+
+  const title = 'Delete the Database';
+  const subtitle = '데이터베이스 삭제';
+  // const content = [
+  //   '⚠️ 데이터베이스 삭제 확인',
+  //   '\u00a0 • 데이터베이스 삭제 시, 모든 정보가 영구적으로 사라집니다.',
+  //   '\u00a0 • 이 작업은 되돌릴 수 없으며 삭제된 데이터는 복구할 수 없습니다.',
+  //   '\u00a0\u00a0\u00a0\u00a0   그래도 삭제하시겠습니까?',
+  // ].join('\n');
+
+  return (
+    <div className="database-delete__container">
+      <div className="database-delete__content">
+        <div className="database-delete__explain">
+          <div className="database-delete__title">
+            <Text content={title} type="title" bold />
+            <Text content={subtitle} type="subtitle" />
+          </div>
+          <div className="database-delete__brief">
+            <Text
+              content="&nbsp; •&nbsp; 데이터베이스 삭제 시, 모든 정보가 영구적으로 사라집니다."
+              type="subtitle"
+            />
+            <Text
+              content="&nbsp; •&nbsp; 이 작업은 되돌릴 수 없으며 삭제된 데이터는 복구할 수 없습니다."
+              type="subtitle"
+            />
+            <Text
+              content="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;그래도 삭제하시겠습니까?"
+              type="subtitle"
+              bold
+              color="danger"
+            />
+          </div>
+        </div>
+        <div className="database-delete__button">
+          <Button
+            size="large"
+            type="button"
+            label="CANCEL"
+            color="other"
+            radius="oval"
+            shadow
+            onClick={onClose}
+          />
+          <Button
+            size="large"
+            type="button"
+            label="DELETE"
+            color="danger"
+            radius="oval"
+            shadow
+            onClick={handleSubmit}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DatabaseDelete;
