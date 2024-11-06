b29cd4fbfead570df812d67a824e17cf383a9425
JUNI <junehee107@naver.com>
2024-11-06 18:11:21 +0900

    🎨 design: 헤더 및 초기화면 css 수정 진행

--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -6,15 +6,16 @@ import Profile from 'components/molecules/profile/Profile';
 import ProfileDetail from 'components/molecules/profiledetail/ProfileDropdown';
 import Text from 'components/atoms/text/Text';
 import Button from 'components/atoms/button/Button';
+import logo from '../../../assets/images/semo_logo_header.svg';
 
 const Header = () => {
   // const token = localStorage.getItem('token');
   // 테스트용 임시 토큰 - true, false 로 테스트!
-  const token = false;
+  const token = true;
   // const userRole = jwt토큰 decode 진행해서 얻기;
   // 테스트용 임시 유저 권한 - super, admin, user 로 테스트!
-  type UserRole = 'super' | 'admin' | 'user';
-  const userRole: UserRole = 'super';
+  type UserRole = 'ROLE_SUPER' | 'ROLE_ADMIN' | 'ROLE_USER';
+  const userRole: UserRole = 'ROLE_SUPER';
   const [isProfileDetailOpen, setIsProfileDetailOpen] = useState(false);
   // 테스트용 임시 유저 이름
   const userName = '태연님';
@@ -27,7 +28,7 @@ const Header = () => {
       label: 'DB관리',
       route: '/database-management',
     },
-    ...(userRole === 'super'
+    ...(userRole === 'ROLE_SUPER'
       ? [
           {
             label: '요청 관리',
@@ -49,14 +50,14 @@ const Header = () => {
   const handleHomeClick = () => {
     let homePath;
     switch (userRole as UserRole) {
-      case 'super':
-        homePath = '/super-home';
+      case 'ROLE_SUPER':
+        homePath = '/';
         break;
-      case 'admin':
-        homePath = '/admin-home';
+      case 'ROLE_ADMIN':
+        homePath = '/';
         break;
-      case 'user':
-        homePath = '/user-home';
+      case 'ROLE_USER':
+        homePath = '/';
         break;
       default:
         homePath = '/';
@@ -66,7 +67,7 @@ const Header = () => {
 
   return (
     <div className="header__container">
-      <div className="header__logo" />
+      <img src={logo} alt="Logo" className="header__logo" />
       {token ? (
         <>
           <div className="header__menu">
@@ -77,7 +78,7 @@ const Header = () => {
             >
               <Text content="홈" type="subtitle" bold />
             </div>
-            {(userRole === 'super' || userRole === 'admin') && (
+            {(userRole === 'ROLE_SUPER' || userRole === 'ROLE_ADMIN') && (
               <div className="header__menu-item">
                 <Text content="관리자 메뉴" type="subtitle" bold />
                 <div className="header__menu-dropdown">
