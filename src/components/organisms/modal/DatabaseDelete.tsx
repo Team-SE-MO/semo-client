0bd1ad658b8114c832a8b79825fada7f0aeb1438
dabeen-jung <ltea99@naver.com>
2024-11-09 07:01:44 +0900

    ♻️ refactor: Database 삭제 모달 tsx 및 css 수정 진행

--- src/components/organisms/modal/DatabaseDelete.tsx
+++ src/components/organisms/modal/DatabaseDelete.tsx
@@ -21,49 +21,49 @@ const DatabaseDelete = ({ isOpen, onClose }: DatabaseDeleteProps) => {
   const subtitle = '데이터베이스 삭제';
 
   return (
-    <div className="database-delete__container">
-      <div className="database-delete__content">
-        <div className="database-delete__explain">
-          <div className="database-delete__title">
-            <Text content={title} type="title" bold />
-            <Text content={subtitle} type="subtitle" />
-          </div>
+    <div className="database-delete__background">
+      <div className="database-delete__modal-container">
+        <div className="database-delete__title">
+          <Text content={title} type="title" bold />
+          <Text content={subtitle} type="subtitle" />
+        </div>
+        <div className="database-delete__content">
           <div className="database-delete__brief">
             <Text
-              content="&nbsp; •&nbsp; 데이터베이스 삭제 시, 모든 정보가 영구적으로 사라집니다."
+              content="• 데이터베이스 삭제 시, 모든 정보가 영구적으로 사라집니다."
               type="subtitle"
             />
             <Text
-              content="&nbsp; •&nbsp; 이 작업은 되돌릴 수 없으며 삭제된 데이터는 복구할 수 없습니다."
+              content="• 이 작업은 되돌릴 수 없어 삭제된 데이터는 복구할 수 없습니다."
               type="subtitle"
             />
             <Text
-              content="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;그래도 삭제하시겠습니까?"
+              content="&nbsp;&nbsp;그래도 삭제하시겠습니까?"
               type="subtitle"
               bold
               color="danger"
             />
           </div>
-        </div>
-        <div className="database-delete__button">
-          <Button
-            size="large"
-            type="button"
-            label="CANCEL"
-            color="other"
-            radius="oval"
-            shadow
-            onClick={onClose}
-          />
-          <Button
-            size="large"
-            type="button"
-            label="DELETE"
-            color="danger"
-            radius="oval"
-            shadow
-            onClick={handleSubmit}
-          />
+          <div className="database-delete__button-group">
+            <Button
+              size="large"
+              type="button"
+              label="CANCEL"
+              color="other"
+              radius="oval"
+              shadow
+              onClick={onClose}
+            />
+            <Button
+              size="large"
+              type="button"
+              label="DELETE"
+              color="danger"
+              radius="oval"
+              shadow
+              onClick={handleSubmit}
+            />
+          </div>
         </div>
       </div>
     </div>
