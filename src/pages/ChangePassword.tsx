78e82cc7e5b7940bad65d97a0df3a356a9c133dc
taeyeon721 <garong0721@gmail.com>
2024-11-14 23:53:25 +0900

    🔨 fix: 이메일 정규표현식 수정

--- src/pages/ChangePassword.tsx
+++ src/pages/ChangePassword.tsx
@@ -9,8 +9,7 @@ import { useNavigate } from 'react-router-dom';
 
 const ChangePassword = () => {
   const emailRegEx =
-    /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/i;
-
+    /^[A-Za-z0-9]+([.-]?[A-Za-z0-9]+)*@[A-Za-z0-9]+([.-]?[A-Za-z0-9]+)*.[A-Za-z]{2,}$/i;
   const [step, setStep] = useState(1);
   const [email, setEmail] = useState('');
   const [emailValid, setEmailValid] = useState(false);
