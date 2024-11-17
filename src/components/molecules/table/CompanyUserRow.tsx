628f5720571e139155629d4f9fc329b46fa4af17
taeyeon721 <garong0721@gmail.com>
2024-11-18 00:30:34 +0900

    :rotating_light: hotfix: 테이블 인덱스 수정

--- src/components/molecules/table/CompanyUserRow.tsx
+++ src/components/molecules/table/CompanyUserRow.tsx
@@ -4,6 +4,7 @@ import Company from 'types/Company';
 // TODO: user 공통 타입 구현
 interface CompanyUserRowProps {
   i: number;
+  pageIndex?: number;
   content: {
     loginId: string;
     role: string;
@@ -15,7 +16,12 @@ interface CompanyUserRowProps {
   onDelete?: (loginId: string) => void;
 }
 // TODO: 삭제 버튼 누르면 delete 요청 보내는 기능 구현
-const CompanyUserRow = ({ i, content, onDelete }: CompanyUserRowProps) => {
+const CompanyUserRow = ({
+  i,
+  pageIndex,
+  content,
+  onDelete,
+}: CompanyUserRowProps) => {
   const [rowData, setRowData] = useState(content);
   useEffect(() => {
     setRowData(content);
@@ -27,7 +33,9 @@ const CompanyUserRow = ({ i, content, onDelete }: CompanyUserRowProps) => {
   };
   return (
     <tr className="table__row">
-      <td className="table__data">{i + 1}</td>
+      <td className="table__data">
+        {pageIndex && (pageIndex - 1) * 10 + i + 1}
+      </td>
       <td className="table__data">{rowData.role.replace('ROLE_', '')}</td>
       <td className="table__data">{rowData.loginId}</td>
       <td className="table__data">{rowData.email}</td>
