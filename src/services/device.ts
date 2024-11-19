2438355b292a13e6c51fa2d3b197be0508ac0a21
heonuk <gitshineit@gmail.com>
2024-11-19 14:39:58 +0900

    ✨ feat: Device 수정, 삭제, 상태 체크 API 구현

--- src/services/device.ts
+++ src/services/device.ts
@@ -23,4 +23,106 @@ const getDeviceList = async (
     .catch(fail);
 };
 
-export { getDeviceList };
+const postDevice = async (
+  deviceAlias: string,
+  dataBaseInfo: {
+    type: string;
+    ip: string;
+    port: number;
+    sid: string;
+    username: string;
+    password: string;
+  },
+  success: (response: AxiosResponse) => void,
+  fail: (error: AxiosError) => void
+): Promise<void> => {
+  const token = localStorage.getItem('accessToken');
+  await api({
+    headers: { Authorization: `Bearer ${token}` },
+    method: 'post',
+    url: '/device',
+    data: {
+      deviceAlias,
+      dataBaseInfo,
+    },
+  })
+    .then(success)
+    .catch(fail);
+};
+
+const testConnection = async (
+  type: string,
+  ip: string,
+  port: number,
+  sid: string,
+  username: string,
+  password: string,
+  success: (response: AxiosResponse) => void,
+  fail: (error: AxiosError) => void
+): Promise<void> => {
+  const token = localStorage.getItem('accessToken');
+  await api({
+    headers: { Authorization: `Bearer ${token}` },
+    method: 'post',
+    url: '/device/hc',
+    data: {
+      type,
+      ip,
+      port,
+      sid,
+      username,
+      password,
+    },
+  })
+    .then(success)
+    .catch(fail);
+};
+
+const patchDevice = async (
+  targetDevice: string,
+  updateDeviceAlias: string,
+  updateDeviceInfo: {
+    type: string;
+    ip: string;
+    port: number;
+    sid: string;
+    username: string;
+    password: string;
+  },
+  success: (response: AxiosResponse) => void,
+  fail: (error: AxiosError) => void
+): Promise<void> => {
+  const token = localStorage.getItem('accessToken');
+  await api({
+    headers: { Authorization: `Bearer ${token}` },
+    method: 'patch',
+    url: '/device',
+    data: {
+      targetDevice,
+      updateDeviceAlias,
+      updateDeviceInfo,
+    },
+  })
+    .then(success)
+    .catch(fail);
+};
+
+const deleteDevice = async (
+  deviceAlias: string,
+  success: (response: AxiosResponse) => void,
+  fail: (error: AxiosError) => void
+): Promise<void> => {
+  const token = localStorage.getItem('accessToken');
+  await api({
+    headers: { Authorization: `Bearer ${token}` },
+    method: 'delete',
+    url: '/device',
+    params: {
+      deviceAlias,
+    },
+  })
+    .then(success)
+    .catch(fail);
+};
+
+export { getDeviceList, postDevice, testConnection, patchDevice, deleteDevice };
